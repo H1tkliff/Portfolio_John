26683f39823455e1cf5d5b8f59eb7ca0844b6d3c
Hitkliff <ilya.gromovoy.02@mail.ru>
2022-06-29 12:44:14 +0300

    add burger, adaptiv, favicon

--- js/main.js
+++ js/main.js
@@ -1,5 +1,37 @@
-$(document).ready(function () {
-  $(".header__burger").click(function () {
-    $(".header__burger").toggleClass("header__active");
+// // самовызывающаяся функция
+// (function () {
+//   // находит первый элемент с селектором который я передам
+//   const header = document.querySelector(".header");
+//   window.onscroll = () => {
+//     // расчитывает расстояние скрола по Y
+//     if (window.pageYOffset > 50) {
+//       header.classList.add("header_active");
+//     } else {
+//       // Если мы вернулись в начало то удалить
+//       header.classList.remove("header_active");
+//     }
+//   };
+// })();
+
+// Burger handler
+(function () {
+  const burgerItem = document.querySelector(".burger");
+  const menu = document.querySelector(".header__nav");
+  const menuCloseOtem = document.querySelector(".header__nav-close");
+  const menuLinks = document.querySelectorAll('.header__link')
+  burgerItem.addEventListener("click", () => {
+    menu.classList.add("header__nav-active");
   });
-});
+  menuCloseOtem.addEventListener("click", () => {
+    menu.classList.remove("header__nav-active");
+  });
+  if(window.innerWidth <= 767){
+    for(let i = 0; i < menuLinks.length; i++){
+      menuLinks[i].addEventListener('click', () => {
+        menu.classList.remove("header__nav-active");
+      })
+
+      
+    }
+  }
+})();
